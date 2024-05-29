05bbb8e2c1e7270bd08e6dd090d5caa3dff87ef9
Johnson Ly <johnsonly2002@hotmail.com>
2024-05-29 11:15:04 +1000

    move nav2_bringup into launch folder

--- setup.py
+++ setup.py
@@ -23,11 +23,11 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
         (os.path.join('share', package_name, 'map'), glob('map/*')),
-        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
-    ] + generate_data_files('share/' + package_name + '/', 'models'),
+    ] + generate_data_files('share/' + package_name + '/', 'models')
+    + generate_data_files('share/' + package_name + '/', 'launch')
+    + generate_data_files('share/' + package_name + '/', 'params'),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Johnson Ly',
@@ -37,8 +37,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'tasker_node        = warehouse_tasker.tasker_node:main',
-            'pather_node        = warehouse_tasker.pather_node:main',
+            'agent_node         = warehouse_tasker.agent_node:main',
             'marker_mapper_node = warehouse_tasker.marker_mapper_node:main',
             'voronoi_graph_node = warehouse_tasker.voronoi_graph_node:main',
             'voronoi_grapher    = warehouse_tasker.voronoi_grapher:main',
