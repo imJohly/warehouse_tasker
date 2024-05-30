6555a72af047354db7fa8f7fa85aeb2b8fc91f66
Johnson Ly <johnsonly2002@hotmail.com>
2024-05-30 17:49:46 +1000

    initial unloader_driver commit

---
+++ warehouse_tasker/unloader_node.py
@@ -0,0 +1,40 @@
+import time
+
+import rclpy
+from rclpy import Node
+
+from std_srvs.srv import SetBool
+
+import RPi.GPIO as GPIO
+
+class UnloaderNode(Node):
+    def __init__(self) -> None:
+        super().__init__('unloader_node')
+
+        self.open: bool = False
+        self.pin = 18
+
+        GPIO.setmode(GPIO.BCM)  # You can also use GPIO.BOARD
+        GPIO.setup(self.pin, GPIO.OUT)
+
+        self.door_service = self.create_service(
+            srv_type=SetBool,
+            srv_name='open_door',
+            callback=self.service_callback
+        )
+
+    def service_callback(self, request, response):
+        if request.data:
+            GPIO.output(self.pin, GPIO.HIGH)
+            time.sleep(1)
+
+        response.success = True
+        return response
+
+def main(args = None) -> None:
+    rclpy.init(args=args)
+    node = UnloaderNode()
+    rclpy.spin(node)
+
+if __name__ == '__main__':
+    main()
