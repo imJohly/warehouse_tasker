66f21a100eb0cb2c8b287bee56f7f8d87448c345
posidon16 <jai.biddulph@gmail.com>
2024-06-04 09:30:33 +1000

    Create explore_rostool.launch.py

---
+++ launch/explore_launch/explore_rostool.launch.py
@@ -0,0 +1,81 @@
+import os
+import launch_ros
+
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node, SetRemap
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # declared_arguments = [
+    #     DeclareLaunchArgument(
+    #         "is_sim",
+    #         default_value="true",
+    #     )]
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    declare_use_sim_time = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value=use_sim_time,
+        description='Use simulation timing'
+    )
+
+    # package_name = 'auto_mapper'
+
+    # auto_mapper = Node(
+    #     package=package_name,
+    #     executable="auto_mapper",
+    #     name="auto_mapper",
+    # )
+
+
+    nav2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('turtlebot3_navigation2'), 
+            'launch',
+            'navigation2.launch.py'
+
+            )
+        )
+    )
+
+    slamtoolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('slam_toolbox'), 
+            'launch',
+            'online_async_launch.py'
+
+            )
+        )
+        # launch_arguments={'use_sim_time': is_sim,}.items()
+    )
+
+
+
+
+    return LaunchDescription([declare_use_sim_time,
+                             nav2_launch,
+                             slamtoolbox_launch,
+                             ])
+
+
+
+
+
+
+
+# def create_slam_toolbox_node() -> object:
+#     launch_file_path = PathJoinSubstitution(
+#         [FindPackageShare(package_name), 'launch', 'online_async_launch.py'])
+#     params_file = PathJoinSubstitution(
+#         [FindPackageShare(package_name), "config", "mapper_params_online_async.yaml"])
+#     return IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource(launch_file_path),
+#         launch_arguments={'use_sim_time': is_sim, 'slam_params_file': params_file, 'map_file_name': map_path}.items()
+#     )
+
