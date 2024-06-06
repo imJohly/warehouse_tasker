c5622d7170f2a0388941330be5ddaf455eb0927a
Callum <14242806@student.uts.edu.au>
2024-06-06 17:33:10 +1000

    param changes

--- launch/gazebo_multi_nav2_world.launch.py
+++ launch/gazebo_multi_nav2_world.launch.py
@@ -32,13 +32,17 @@ def generate_launch_description():
 
     # Names and poses of the robots
     robots = [
-         {'name': 'tb2', 'x_pose': '-1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
-        #{'name': 'tb2', 'x_pose': '-1.5', 'y_pose': '0.5', 'z_pose': 0.01},
-    ]
+        {'name': 'tb1', 'x_pose': '-1.5', 'y_pose': '-0.5', 'z_pose': 0.01},
+        {'name': 'tb2', 'x_pose': '-1.5', 'y_pose': '-1.0', 'z_pose': 0.01},
+ #       {'name': 'tb3', 'x_pose': '1.5', 'y_pose': '-0.5', 'z_pose': 0.01}
+        # {'name': 'tb4', 'x_pose': '1.5', 'y_pose': '0.5', 'z_pose': 0.01},
+        # ...
+        # ...
+        ]
 
     TURTLEBOT3_MODEL = 'waffle'
 
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     declare_use_sim_time = DeclareLaunchArgument(
         name='use_sim_time', default_value=use_sim_time, description='Use simulator time'
     )
@@ -53,9 +57,7 @@ def generate_launch_description():
         name='enable_rviz', default_value=enable_rviz, description='Enable rviz launch'
     )
     
-
-    
-#    turtlebot3_multi_robot = get_package_share_directory('turtlebot3_multi_robot')
+    turtlebot3_multi_robot = get_package_share_directory('turtlebot3_multi_robot')
 
     package_dir = get_package_share_directory('warehouse_tasker')
     nav_launch_dir = os.path.join(package_dir, 'launch', 'nav2_bringup')
@@ -68,13 +70,30 @@ def generate_launch_description():
         description='Full path to the RVIZ config file to use')
 
     urdf = os.path.join(
-        package_dir, 'urdf', 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
+        turtlebot3_multi_robot, 'urdf', 'turtlebot3_' + TURTLEBOT3_MODEL + '.urdf'
+    )
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_multi_robot'),
+        'worlds', 'warehouse_world.world')
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items(),
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')
+        ),
     )
 
     params_file = LaunchConfiguration('nav_params_file')
     declare_params_file_cmd = DeclareLaunchArgument(
         'nav_params_file',
-        default_value=os.path.join(get_package_share_directory('warehouse_tasker'), 'params', 'nav2_params.yaml'),
+        default_value=os.path.join(package_dir, 'params', 'nav2_sim_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
     
      
@@ -83,6 +102,8 @@ def generate_launch_description():
     ld.add_action(declare_enable_rviz)
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_params_file_cmd)
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
  
     remappings = [('/tf', 'tf'),
                   ('/tf_static', 'tf_static')]
@@ -90,7 +111,7 @@ def generate_launch_description():
         executable='map_server',
         name='map_server',
         output='screen',
-        parameters=[{'yaml_filename': os.path.join(get_package_share_directory('warehouse_tasker'), 'map', 'map_real_2.yaml'),
+        parameters=[{'yaml_filename': os.path.join(get_package_share_directory('warehouse_tasker'), 'map', 'map.yaml'),
                      },],
         remappings=remappings)
 
@@ -112,12 +133,39 @@ def generate_launch_description():
     # will get be published on root '/' namespace
     remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
 
-    # last_action = None
+    last_action = None
 
     # Spawn turtlebot3 instances in gazebo
     for robot in robots:
 
         namespace = [ '/' + robot['name'] ]
+
+        # Create state publisher node for that instance
+        turtlebot_state_publisher = Node(
+            package='robot_state_publisher',
+            namespace=namespace,
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time,
+                            'publish_frequency': 10.0}],
+            remappings=remappings,
+            arguments=[urdf],
+        )
+
+        # Create spawn call
+        spawn_turtlebot3_burger = Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-file', os.path.join(turtlebot3_multi_robot,'models', 'turtlebot3_' + TURTLEBOT3_MODEL, 'model.sdf'),
+                '-entity', robot['name'],
+                '-robot_namespace', namespace,
+                '-x', robot['x_pose'], '-y', robot['y_pose'],
+                '-z', '0.01', '-Y', '0.0',
+                '-unpause',
+            ],
+            output='screen',
+        )
 
         bringup_cmd = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
@@ -133,15 +181,31 @@ def generate_launch_description():
                                         get_package_share_directory('nav2_bt_navigator'),
                                         'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
                                     'autostart': 'true',
-                                    'use_sim_time': use_sim_time, 'log_level': 'warn'}.items(),
+                                    'use_sim_time': use_sim_time, 'log_level': 'warn'}.items()
                                     )
 
-        # if last_action is None:
+        if last_action is None:
             # Call add_action directly for the first robot to facilitate chain instantiation via RegisterEventHandler
-        ld.add_action(bringup_cmd)
+            ld.add_action(turtlebot_state_publisher)
+            ld.add_action(spawn_turtlebot3_burger)
+            ld.add_action(bringup_cmd)
+
+        else:
+            # Use RegisterEventHandler to ensure next robot creation happens only after the previous one is completed.
+            # Simply calling ld.add_action for spawn_entity introduces issues due to parallel run.
+            spawn_turtlebot3_event = RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=last_action,
+                    on_exit=[spawn_turtlebot3_burger,
+                            turtlebot_state_publisher,
+                            bringup_cmd],
+                )
+            )
+
+            ld.add_action(spawn_turtlebot3_event)
 
         # Save last instance for next RegisterEventHandler
-        # last_action = bringup_cmd
+        last_action = spawn_turtlebot3_burger
     ######################
 
     ######################
@@ -179,19 +243,17 @@ def generate_launch_description():
 
         # Use RegisterEventHandler to ensure next robot rviz launch happens 
         # only after all robots are spawned
-        # post_spawn_event = RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=last_action,
-        #         # on_exit=[initial_pose_cmd, rviz_cmd, drive_turtlebot3_burger],
-        #         on_exit=[rviz_cmd, drive_turtlebot3_burger],
-        #     )
-        # )
+        post_spawn_event = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=last_action,
+                on_exit=[initial_pose_cmd, rviz_cmd, drive_turtlebot3_burger],
+            )
+        )
 
         # Perform next rviz and other node instantiation after the previous intialpose request done
-        ld.add_action(initial_pose_cmd)
-        ld.add_action(rviz_cmd)
-        ld.add_action(drive_turtlebot3_burger)
-        # ld.add_action(post_spawn_event)
+        last_action = initial_pose_cmd
+
+        ld.add_action(post_spawn_event)
         ld.add_action(declare_params_file_cmd)
     ######################
 
