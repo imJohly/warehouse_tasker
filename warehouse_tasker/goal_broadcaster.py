1c756b3bc86ea77fc135afbba9fde9d87acbf388
Johnson Ly <johnsonly2002@hotmail.com>
2024-06-14 22:42:10 +1000

    goal broadcaster now broadcasts multiple markers

--- warehouse_tasker/goal_broadcaster.py
+++ warehouse_tasker/goal_broadcaster.py
@@ -1,14 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.clock import Time
 
+from std_msgs.msg import ColorRGBA
 import yaml
 
 from tf2_ros import Buffer, StaticTransformBroadcaster, TransformListener
 
-from geometry_msgs.msg import TransformStamped
-
-# TODO: Broadcast any goal Tfs to a marker/goal_visual topic
+from geometry_msgs.msg import Point, Quaternion, TransformStamped, Vector3
+from visualization_msgs.msg import Marker, MarkerArray
 
 class GoalBroadcaster(Node):
 
@@ -19,50 +18,77 @@ class GoalBroadcaster(Node):
         self.listener = TransformListener(self.tf_buffer, self)
         self.static_tf_broadcaster = StaticTransformBroadcaster(self)
 
-        self.declare_parameter('marker_index', 0)
+        self.goal_marker_publisher = self.create_publisher(MarkerArray, 'goal_markers', 5)
+
         self.declare_parameter('marker_yaml_path', '')
+        self.declare_parameter('publish_visual', True)
 
         marker_yaml_path = self.get_parameter('marker_yaml_path').get_parameter_value().string_value
-        self.import_goals(marker_yaml_path)
 
+        self.marker_locations = self.import_goals(marker_yaml_path)
         self.transfer_transforms()
+        self.publish_visual_markers()
+        # self.create_timer(60.0, self.publish_visual_markers)
 
     def import_goals(self, file_path: str):
-        self.marker_locations = {}
+        """Imports goal locations from a YAML file"""
+        marker_locations = {}
         with open(file_path, 'r') as stream:
-            self.marker_locations = yaml.safe_load(stream)
+            marker_locations = yaml.safe_load(stream)
         self.get_logger().info('Imported marker locations.')
 
+        return marker_locations
+
     def transfer_transforms(self):
-        # try:
-        index = self.get_parameter('marker_index').get_parameter_value().integer_value
+        for i in self.marker_locations['positions']:
+            try:
+                marker_transform = TransformStamped()
+                marker_transform.header.frame_id = 'map'
+                marker_transform.child_frame_id = f'marker{i}'
 
-        marker_transform = TransformStamped()
-        marker_transform.header.frame_id = 'map'
-        marker_transform.child_frame_id = f'marker{index}'
+                x = self.marker_locations['positions'][i]['translation'][0]
+                y = self.marker_locations['positions'][i]['translation'][1]
+                z = self.marker_locations['positions'][i]['translation'][2]
 
-        x = self.marker_locations['positions'][index]['translation'][0]
-        y = self.marker_locations['positions'][index]['translation'][1]
-        z = self.marker_locations['positions'][index]['translation'][2]
+                self.get_logger().info(f"{i}: {self.marker_locations['positions'][i]}")
 
-        self.get_logger().info(f"{index}: {self.marker_locations['positions'][index]}")
+                marker_transform.transform.translation.x = float(x)
+                marker_transform.transform.translation.y = float(y)
+                marker_transform.transform.translation.z = float(z)
 
-        marker_transform.transform.translation.x = float(x)
-        marker_transform.transform.translation.y = float(y)
-        marker_transform.transform.translation.z = float(z)
+                marker_transform.transform.rotation.w = 1.0
 
-        marker_transform.transform.rotation.w = 1.0
+                self.static_tf_broadcaster.sendTransform(marker_transform)
+                self.get_logger().info(f'Broadcasting marker{i}')
+            except BaseException as e:
+                self.get_logger().warn(f"Error transferring transform: {e}")
 
-        self.static_tf_broadcaster.sendTransform(marker_transform)
-        self.get_logger().info(f'Broadcasting marker{index} with translation {marker_transform.transform.translation}')
+    def publish_visual_markers(self) -> None:
+        array_msg = MarkerArray()
 
-        # except BaseException as e:
-        #     self.get_logger().warn(f"Error transferring transform: {e}")
+        for i in self.marker_locations['positions']:
+            position = self.marker_locations['positions'][i]['translation']
+            marker_msg = Marker()
+            marker_msg.header.frame_id = 'map'
+            marker_msg.type = Marker.ARROW
+            marker_msg.id = i
+            marker_msg.action = Marker.ADD
+
+            marker_msg.pose.position = Point(x=position[0], y=position[1], z=position[2]+0.6)
+            marker_msg.pose.orientation = Quaternion(x=0.0, y=0.707, z=0.0, w=0.707)
+            marker_msg.scale = Vector3(x=0.5, y=0.1, z=0.1)
+            marker_msg.color = ColorRGBA(r=0.0, g=1.0, b=0.5, a=0.5)
+
+            array_msg.markers.append(marker_msg)
+            self.get_logger().info(f'Set up marker {i}')
+
+        self.goal_marker_publisher.publish(array_msg)
+        self.get_logger().info('Published marker array!')
 
 def main():
     rclpy.init()
     
-    node = DropZoneBroadcaster()
+    node = GoalBroadcaster()
     rclpy.spin(node)
 
 if __name__ == '__main__':
