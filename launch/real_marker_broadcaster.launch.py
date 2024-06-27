8ea954d85d64cc1fed1610074bc70ceb368cb585
Johnson Ly <johnsonly2002@hotmail.com>
2024-06-27 10:39:56 +1000

    sync real_marker_broadcaster.launch.py

--- launch/real_marker_broadcaster.launch.py
+++ launch/real_marker_broadcaster.launch.py
@@ -16,35 +16,13 @@ def generate_launch_description():
         'marker_locations.yaml'
     )
 
-    config = {}
-    with open(marker_config, 'r') as stream:
-        config = yaml.safe_load(stream)
-    number_of_markers = len(list(config['positions']))
-
-    marker_nodes = []
-    for i in range(number_of_markers):
-        marker_nodes.append(
-            Node(
-                package='warehouse_tasker',
-                executable='drop_zone_broadcaster',
-                output='screen',
-                parameters=[{'marker_yaml_path': marker_config,
-                             'marker_index': i}],
-                remappings=[('/tf_static', '/tb1/tf_static')]
-            )
-        )
-
-    marker_nodes = []
-    for i in range(number_of_markers):
-        marker_nodes.append(
-            Node(
-                package='warehouse_tasker',
-                executable='drop_zone_broadcaster',
-                output='screen',
-                parameters=[{'marker_yaml_path': marker_config,
-                             'marker_index': i}]
-            )
-        )
+    broadcaster_node = Node(
+        package='warehouse_tasker',
+        executable='goal_broadcaster',
+        output='screen',
+        parameters=[{'marker_yaml_path': marker_config}]
+    )
 
     return LaunchDescription([
-    ] + marker_nodes)
+        broadcaster_node
+    ])
